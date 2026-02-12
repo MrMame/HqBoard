bcb855ea7ed85cdb0916d921732fad287a4786ce
MMe <51000524+MrMame@users.noreply.github.com>
2026-02-12 17:31:58 +0100

    - Changed to Typescript Projekt setup

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const button = document.getElementById("board");
+button.addEventListener("click", () => {
+    alert("Hallo von TypeScript!");
+});
+//# sourceMappingURL=main.js.map
---
+++ src/scripts/main.ts
@@ -0,0 +1,5 @@
+const button = document.getElementById("board") as HTMLButtonElement;
+
+button.addEventListener("click", () => {
+    alert("Hallo von TypeScript!");
+});
