77bac7b6cda39d47ffb206c33d9a9f895a192c45
MrMame <hereAtLaptop@github.com>
2026-02-15 18:28:51 +0100

    - Add Type

--- www/scripts/UI/Factories/ToolboxMarkerViewFactory.ts
+++ www/scripts/UI/Factories/ToolboxMarkerViewFactory.ts
@@ -4,7 +4,7 @@ export class ToolboxMarkerViewFactory{
 
     private static TOOLBOX_MARKER_TYPE = "marker";
 
-    public static createToolboxMarkerView(letter:string){
+    public static createToolboxMarkerView(letter:string):ToolboxMarkerView{
         return new ToolboxMarkerView(this.TOOLBOX_MARKER_TYPE,letter);
     }
 }
